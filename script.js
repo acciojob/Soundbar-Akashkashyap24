a7c98da4c03199af0ffbe0ed197665608c950110
Akashkashyap24 <144315919+Akashkashyap24@users.noreply.github.com>
2024-02-07 02:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let btns = document.querySelectorAll('.btn');
 let stop = document.querySelector('#stop');
+let currentAudio = null; // to keep track of the currently playing audio
 
 btns.forEach((btn) => {
   btn.addEventListener('click', () => {
-    new Audio('sounds/' + btn.innerText + '.mp3').play();
+    if (currentAudio) { // if there's an audio playing, stop it
+      currentAudio.pause();
+    }
+    currentAudio = new Audio('sounds/' + btn.innerText + '.mp3');
+    currentAudio.play();
   });
-	stop.addEventListener('click', ()=>{
-	Audio().pause();
-})
+});
+
+stop.addEventListener('click', () => {
+  if (currentAudio) { // if there's an audio playing, stop it
+    currentAudio.pause();
+  }
 });
